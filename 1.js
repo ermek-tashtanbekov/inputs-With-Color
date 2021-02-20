1b86da9b843992cb1bd4f2881b885711bf2f9c80
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-02-20 11:56:07 +0600

    link querySelectorAll input button

---
+++ 1.js
@@ -0,0 +1,4 @@
+let button = document.querySelector('button');
+let namee = document.querySelector('#name');
+let message = document.querySelector('#message');
+let color = document.querySelector('#color')
