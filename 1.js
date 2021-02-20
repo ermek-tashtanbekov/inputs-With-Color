5879f7f4e48b1066bad28cb0ffc28c8b88848110
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-02-20 11:59:46 +0600

    added button listenr click add let createspans div

--- 1.js
+++ 1.js
@@ -1,4 +1,11 @@
 let button = document.querySelector('button');
 let namee = document.querySelector('#name');
 let message = document.querySelector('#message');
-let color = document.querySelector('#color')
+let color = document.querySelector('#color');
+
+button.addEventListener('click', function(){
+    let div = document.createElement('div');
+    let spanName = document.createElement('span');
+    let spanColor = document.createElement('span');
+    
+})
